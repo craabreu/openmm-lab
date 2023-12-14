f50ab0e9aa782f6151f76ff9ba1ef7447972e627
Charlles Abreu <charlles@redesignscience.com>
2023-12-14 15:53:04 -0500

    Included reference platform tests

---
+++ platforms/reference/tests/TestReferenceExtendedCustomCVForce.cpp
@@ -0,0 +1,15 @@
+/* -------------------------------------------------------------------------- *
+ *                             OpenMM Laboratory                              *
+ *                             =================                              *
+ *                                                                            *
+ * A plugin for testing low-level code implementation for OpenMM.             *
+ *                                                                            *
+ * Copyright (c) 2023 Charlles Abreu                                          *
+ * https://github.com/craabreu/openmm-lab                                     *
+ * -------------------------------------------------------------------------- */
+
+#include "ReferenceOpenMMLabTests.h"
+#include "TestExtendedCustomCVForce.h"
+
+void runPlatformTests() {
+}
